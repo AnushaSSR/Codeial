ecec15122f3cfc129c1c86ef3b305684da5dc7fe
Anusha <nssranusha@yaho..co.in>
2022-07-01 05:16:39 +0530

    first commit : create directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,15 @@
+// to get the expresss
+
+const express = require('express');
+const app = express();
+
+//to get the port, default is 80, we will go by 8000
+const port= 8000;
+
+// make the app listen to the port
+app.listen(port,function(err){
+    if(err) {
+        console.log(`Error in loading the server : ${err}`);
+    }
+    console.log(`Server is up and running: ${port}`);
+})

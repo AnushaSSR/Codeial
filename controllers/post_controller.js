2007dbbd7be81158d7fdf5dee0d8dd72ad327cfb
Anusha <nssranusha@yaho..co.in>
2022-07-01 05:55:16 +0530

    Assignment:Added routes and contoller for posts

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,7 +1,3 @@
-module.exports.profile = function(req,res){
-    return res.end('<h1> User Profile </h1>');
-}
-
 
 module.exports.posts = function(req, res) {
     return res.end('<h1> User posts page </h1>');
--- routes/user.js
+++ routes/user.js
@@ -5,8 +5,10 @@ const router = express.Router();
 
 
 const user_controller = require('../controllers/user_controller');
-
-
+const post_controller= require('../controllers/post_controller');
+// create a route to profile
 router.get('/profile', user_controller.profile);
+// assignmebt to create a post route
+router.get('/posts', post_controller.posts);
 
 module.exports =router;
